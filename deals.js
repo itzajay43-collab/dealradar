4e260b9a0e2898ec731c02480753061f5024e2fd
itzajay43-collab <itzajay43@gmail.com>
2026-06-02 16:27:44 +0530

    Add 10 products with affiliate support

--- deals.js
+++ deals.js
@@ -6,7 +6,8 @@ oldPrice: "1499",
 price: "799",
 discount: "47% OFF",
 image: "assets/Boat Airdopes 141 PNG.png",
-category: "electronics"
+category: "electronics",
+link: "https://fkrt.co/0yRpOe"
 },
 
 {
@@ -15,7 +16,8 @@ oldPrice: "2499",
 price: "999",
 discount: "60% OFF",
 image: "assets/Noise Smartwatch PNG.png",
-category: "electronics"
+category: "electronics",
+link: "https://fkrt.co/ukxi0p"
 },
 
 {
@@ -24,7 +26,8 @@ oldPrice: "1999",
 price: "899",
 discount: "55% OFF",
 image: "assets/Fire-Boltt Ninja PNG.png",
-category: "electronics"
+category: "electronics",
+link: "https://fkrt.co/E8wJCX"
 },
 
 {
@@ -33,7 +36,8 @@ oldPrice: "3999",
 price: "1499",
 discount: "62% OFF",
 image: "assets/Noise Smartwatch PNG.png",
-category: "electronics"
+category: "electronics",
+link: "https://fkrt.co/tSWYje"
 },
 
 {
@@ -42,7 +46,58 @@ oldPrice: "1999",
 price: "799",
 discount: "60% OFF",
 image: "assets/electric-kettle.png",
-category: "home"
+category: "home",
+link: "https://fkrt.co/dmzeN1"
+},
+
+{
+name: "Philips Trimmer BT3101",
+oldPrice: "1495",
+price: "949",
+discount: "36% OFF",
+image: "assets/trimmer.png",
+category: "electronics",
+link: "https://fkrt.co/4YhM5K"
+},
+
+{
+name: "Samsung 25W Charger",
+oldPrice: "1699",
+price: "999",
+discount: "41% OFF",
+image: "assets/charger.png",
+category: "electronics",
+link: "https://fkrt.co/Q0MqyO"
+},
+
+{
+name: "boAt Rockerz 255 Pro+",
+oldPrice: "3990",
+price: "899",
+discount: "77% OFF",
+image: "assets/rockerz255.png",
+category: "electronics",
+link: "https://fkrt.co/vc2k77"
+},
+
+{
+name: "JBL Go 3 Speaker",
+oldPrice: "3999",
+price: "2499",
+discount: "37% OFF",
+image: "assets/jblgo3.png",
+category: "electronics",
+link: "https://fkrt.co/xbjdFN"
+},
+
+{
+name: "Pigeon Mini Chopper",
+oldPrice: "545",
+price: "183",
+discount: "66% OFF",
+image: "assets/chopper.png",
+category: "home",
+link: "https://fkrt.co/juANfO"
 }
 
 ];
