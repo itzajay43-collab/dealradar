d1d1a7f860147d1069ef1a6d0716642dde904d34
itzajay43-collab <itzajay43@gmail.com>
2026-06-02 14:07:33 +0530

    Create deals database

---
+++ deals.js
@@ -0,0 +1,28 @@
+const deals = [
+{
+name: "Boat Airdopes 141",
+oldPrice: "1499",
+price: "799",
+discount: "47% OFF",
+image: "assets/Boat Airdopes 141 PNG.png",
+category: "electronics"
+},
+
+{
+name: "Noise Smartwatch",
+oldPrice: "2499",
+price: "999",
+discount: "60% OFF",
+image: "assets/Noise Smartwatch PNG.png",
+category: "electronics"
+},
+
+{
+name: "Fire-Boltt Ninja",
+oldPrice: "1999",
+price: "899",
+discount: "55% OFF",
+image: "assets/Fire-Boltt Ninja PNG.png",
+category: "electronics"
+}
+];
